094013242cc0cc422d7c3b7d11dcb56f406b7a7d
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2025-08-01 21:54:39 -0400

    fix: login
    Estaba usando sameSite lax lo que impedía que se creara la sección en orígenes distintos.

--- server/node-server/src/controllers/auth.ts
+++ server/node-server/src/controllers/auth.ts
@@ -58,7 +58,7 @@ export const login = async (req: Request, res: Response) => {
 		res.cookie("token", token, {
 			httpOnly: true,
 			secure: process.env.NODE_ENV === "production", // importante en producción
-			sameSite: "lax", // puedes usar "strict" o "none" según tu front
+			sameSite: "none", // puedes usar "strict" o "none" según tu front
 			maxAge: 1000 * 60 * 60 * 24, // 24 horas
 		});
 
