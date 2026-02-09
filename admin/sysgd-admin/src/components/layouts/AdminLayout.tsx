5d42ac786213d0ec48d1b7400a6db889ec948fa8
Lazaro Yunier <156694602+lazaroysr96@users.noreply.github.com>
2026-02-09 10:20:17 -0500

    Improve admin animated background contrast

--- admin/sysgd-admin/src/components/layouts/AdminLayout.tsx
+++ admin/sysgd-admin/src/components/layouts/AdminLayout.tsx
@@ -4,13 +4,16 @@ import { AdminSidebar } from "../../components/Sidebar"
 
 export default function AdminLayout() {
 	return (
-		<div className="min-h-screen admin-animated-bg">
-			<AdminSidebar />
-			<main className="lg:pl-64">
-				<div className="p-6 pt-16 lg:pt-6">
-					<Outlet />
-				</div>
-			</main>
+		<div className="min-h-screen relative overflow-hidden bg-slate-950">
+			<div className="absolute inset-0 admin-animated-bg" aria-hidden="true" />
+			<div className="relative z-10">
+				<AdminSidebar />
+				<main className="lg:pl-64">
+					<div className="p-6 pt-16 lg:pt-6">
+						<Outlet />
+					</div>
+				</main>
+			</div>
 		</div>
 	)
 }
